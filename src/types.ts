161087d6f6f5d0433fc2d198d97d934fbd574bf3
bgraokmush <saltuk.bugraokumus@gmail.com>
2026-06-13 20:21:28 +0300

    Add initial project files including .gitignore, package.json, TypeScript configuration, and type definitions

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export type FieldType =
+    | 'u8' | 'u16' | 'u32' | 'u64' | 'u128'
+    | 'i8' | 'i16' | 'i32' | 'i64' | 'i128'
+    | 'bool'
+    | 'publicKey'
+    | 'string'
+    | 'bytes';
+
+export interface FieldDefinition {
+    type: FieldType;
+    offset: number;
+}
+
+
+export interface ModelDefinition {
+    discriminator: number[];
+    fields: Record<string, FieldDefinition>;
+}
+
+export type InferFieldType<T extends FieldType> =
+    T extends 'u64' | 'u128' | 'i64' | 'i128' ? bigint :
+    T extends 'u8' | 'u16' | 'u32' | 'i8' | 'i16' | 'i32' ? number :
+    T extends 'bool' ? boolean :
+    T extends 'publicKey' ? string :
+    T extends 'string' | 'bytes' ? string :
+    never;
+
+
+export type InferModel<M extends ModelDefinition> = {
+    [K in keyof M['fields']]: InferFieldType<M['fields'][K]['type']>
+} & { address: string };  // her account'ın PDA adresi de olacak
